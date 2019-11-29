6d8e9d21225436424808b65938b51172164c230e
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-11-29 14:53:32 +0530

    Update Task5.scala

--- Task5.scala
+++ Task5.scala
@@ -1,48 +1,34 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object Task5 {
+object Task6 {
 	def main(args: Array[String]) = {
 	
-		val appName = "Task 5"
+		val appName = "Task 6"
 		val spark = SparkSession.builder.
-			master("local").
-			appName(appName).
-			getOrCreate()
+			.enableHiveSupport()
+			.master("local")
+			.appName(appName)
+			.getOrCreate()
 		val sc = spark.sparkContext
 		
 		try {
-		
+					
+			// Load the attached dataset into a dataframe.
 			val filePath = args(0)
-			val myLog = sc.textFile("filePath")
+			val usDF = spark.read.format("csv").option("inferSchema","true").option("header","true").load(filePath)
 
-			// Count the number of words in the file.
-			val totalWords = myLog.flatMap(_.split("""\s+""")).count()
-			println(totalWords)
+			// Remove the duplicate rows from the Dataset.
+			val withoutDup = usDF.dropDuplicates()
+			withoutDup.count
 
-			// Count the number of line in the file.
-			val totalLines = myLog.count
-			println(totalLines)
-			
-			// Count the number of lines having log level as “ERROR”
-			val errorCount = myLog.filter(_.contains("ERROR")).count
-			println(errorCount)
+			// Remove the rows having invalid email ids.
 
-			// Count the number of words starting with “com.apple.”
-			val comApp = myLog.flatMap(_.split("""\s+""")).filter(_.startsWith("com.apple")).count()
-			println(comApp)
 
-			// Create different files for Warning, error and INFo logs in HDFS. 
-			val hdfsPATH = "/user/training/log"
-			
-			val errorLog = myLog.filter(_.contains("ERROR"))
-			errorLog.saveAsTextFile(hdfsPATH + "/error")
-			
-			val warnLog = myLog.filter(_.contains("WARN"))
-			warnLog.saveAsTextFile(hdfsPATH + "/warning")
-			
-			val infoLog = myLog.filter(_.contains("INFO"))
-			infoLog.saveAsTextFile(hdfsPATH + "/info")
+			// Remove http:// from the web column
+			val webDF = withoutDup.withColumn("web_mod",regexp_replace($"web","http://","")).drop($"web")
+
+			// Write the final result to a Hive ORC table (Table should be Partitioned based on state column)
 			
 		} finally {
 			spark.stop()
