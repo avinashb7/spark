8e7049e74173d50e10676a9f23ba8ee0fa53f1f3
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-11-29 14:52:55 +0530

    Update Task5.scala

--- Task5.scala
+++ Task5.scala
@@ -18,20 +18,31 @@ object Task5 {
 
 			// Count the number of words in the file.
 			val totalWords = myLog.flatMap(_.split("""\s+""")).count()
+			println(totalWords)
 
 			// Count the number of line in the file.
 			val totalLines = myLog.count
-
+			println(totalLines)
+			
 			// Count the number of lines having log level as “ERROR”
 			val errorCount = myLog.filter(_.contains("ERROR")).count
+			println(errorCount)
 
 			// Count the number of words starting with “com.apple.”
 			val comApp = myLog.flatMap(_.split("""\s+""")).filter(_.startsWith("com.apple")).count()
+			println(comApp)
 
 			// Create different files for Warning, error and INFo logs in HDFS. 
+			val hdfsPATH = "/user/training/log"
+			
 			val errorLog = myLog.filter(_.contains("ERROR"))
+			errorLog.saveAsTextFile(hdfsPATH + "/error")
+			
 			val warnLog = myLog.filter(_.contains("WARN"))
+			warnLog.saveAsTextFile(hdfsPATH + "/warning")
+			
 			val infoLog = myLog.filter(_.contains("INFO"))
+			infoLog.saveAsTextFile(hdfsPATH + "/info")
 			
 		} finally {
 			spark.stop()
