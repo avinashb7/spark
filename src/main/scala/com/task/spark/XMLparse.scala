48a2edf087ec2f2a2a53354ba53ddf4eb75069bf
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-12-14 23:32:34 +0530

    Update XMLparse.scala

--- src/main/scala/com/task/spark/XMLparse.scala
+++ src/main/scala/com/task/spark/XMLparse.scala
@@ -4,26 +4,75 @@ import org.apache.spark.sql.SparkSession
 import com.databricks.spark.xml.util.XmlFile
 import org.apache.spark.sql.functions.explode
 
-
 object XMLparse {
 
   def main(args: Array[String]): Unit = {
 
+    //Setting app name.
     val appName = "XMLParse"
+    
+    //Create spark session.
     val spark = SparkSession.builder
       .master("local")
       .appName(appName)
       .getOrCreate()
 
+    //Import implicits to use "$" instead of column names.
     import spark.implicits._
 
     try {
+      //Read input file as 1st arguement in spark-submit command
       val inputFile = args(0)
+      
+      //Read the data using databricks package, import the package and add it in pom.xml
       val xmlFile = spark.read.format("com.databricks.spark.xml")
         .option("rowTag", "taxa")
         .load(inputFile)
-      val xmlFlat = xmlFile.withColumn("taxon_col", explode($"taxon")).drop($"taxon")
-      xmlFlat.show
+      
+      //Verify the Schema
+      xmlFile.printSchema
+      /*
+      root
+       |-- _id: string (nullable = true)
+       |-- taxon: array (nullable = true)
+       |    |-- element: struct (containsNull = true)
+       |    |    |-- _VALUE: string (nullable = true)
+       |    |    |-- _id: string (nullable = true)
+       |    |    |-- _idref: string (nullable = true) 
+      */
+      
+      // Print the result to verify the output
+      xmlFile.show(5)
+      /*
+        +-------------------+--------------------+
+        |                _id|               taxon|
+        +-------------------+--------------------+
+        |               taxa|[[, D_asper,], [,...|
+        |arizonae mojavensis|[[,, D_arizonae],...|
+        |            ingroup|[[,, D_a_american...|
+        |        montana sbg|[[,, D_borealise]...|
+        |        picticornis|[[,, D_cyrtoloma]...|
+        +-------------------+--------------------+
+      */
+      
+      //Flatten the "taxon" column using explode and drop the original column if required.
+      val xmlFlat = xmlFile.withColumn("taxon_col",explode($"taxon._id")).drop($"taxon")
+
+      xmlFlat.show(5)
+      /* Final output
+        +----+------------+
+        | _id|   taxon_col|
+        +----+------------+
+        |taxa|     D_asper|
+        |taxa|    D_daruma|
+        |taxa|D_latifshahi|
+        |taxa|  D_hirtipes|
+        |taxa|D_polychaeta|
+        +----+------------+
+      */
+      
+      //Write to any file or database if required.
+      
     } finally {
       spark.stop()
     }
